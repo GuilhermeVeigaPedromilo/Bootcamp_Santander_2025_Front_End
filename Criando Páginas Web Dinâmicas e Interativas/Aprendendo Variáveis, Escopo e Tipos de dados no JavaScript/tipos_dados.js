7d47672795a055fbe7b9a776367c91aaccd0e2c7
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2025-08-21 23:33:13 -0300

    update

---
+++ Criando Páginas Web Dinâmicas e Interativas/Aprendendo Variáveis, Escopo e Tipos de dados no JavaScript/tipos_dados.js
@@ -0,0 +1,18 @@
+// bool
+// null
+// undefined
+// number
+// str
+// synbol
+// obj
+
+// false
+console.log(!!0)
+
+// true
+console.log(!!2)
+
+// Realizando cálculos
+
+console.log(0.1 - 0.3)
+// O resultado é -0.19,99999999, para realizar cálculos monetários ou comuns do dia a dia, utilize decimal
